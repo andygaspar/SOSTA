65088d4b0f3befa603547a4db1e890c272175c29
andrea <andygaspar@libero.it>
2021-04-07 13:22:44 +0200

    parallelizzato

--- series.py
+++ series.py
@@ -1,26 +1,21 @@
 import pandas as pd
 from multiprocessing import Pool
+import time
 
 
 def compute_series(tup):
-    airp, df_in, proc_num = tup
+    df_in, airp, num = tup
     df_to_append = pd.DataFrame(columns=df_in.columns)
     num_series = 0
-    num_airport = 0
-    len_airp = airp.shape[0]
-    for airport in airp:
-        air = df_in[(df_in["departure"] == airport) ^ (df_in["arrival"] == airport)]
-        for i in range(7):
-            print(proc_num, len_airp,":", num_airport, i)
-            hhh = air[air["week day"] == i]
-            for cs in hhh["callsign"]:
-                same_call = hhh[hhh["callsign"] == cs].copy()
-                if same_call.shape[0] > 4:
-                    same_call["series"] = num_series
-                    df_to_append = pd.concat([df_to_append, same_call], ignore_index=True)
-                    num_series += 1
-        num_airport += 1
-    print("done")
+    for i in range(7):
+        hhh = df_in[df_in["week day"] == i]
+        for cs in hhh["callsign"].unique():
+            same_call = hhh[hhh["callsign"] == cs].copy()
+            if same_call.shape[0] > 4:
+                same_call["series"] = num_series
+                df_to_append = pd.concat([df_to_append, same_call], ignore_index=True)
+                num_series += 1
+    print(num)
     return df_to_append
 
 
@@ -28,22 +23,21 @@ if __name__ == '__main__':
     df = pd.read_csv("data/summer_2019.csv")
     airports = pd.read_csv("data/airports.csv")
 
-    num_procs = 16
-    pool = Pool(num_procs)
-    len_slice = airports.shape[0]//num_procs
-    split_df_idx = [i*len_slice for i in range(num_procs)] + [airports.shape[0]]
-
-
     split_df = []
-    for i in range(num_procs):
-        partial_idx = airports["airport"][split_df_idx[i]:split_df_idx[i+1]]
-        print(partial_idx)
-        split_df.append((partial_idx.copy(),
-                         df[(df["departure"].isin(partial_idx)) ^ (df["arrival"].isin(partial_idx))].copy(), i))
-
-    final_df = pd.concat(pool.map(compute_series, split_df))
+    ind = 0
+    for airport in airports["airport"]:
+        split_df.append((df[(df["departure"] == airport) ^ (df["arrival"] == airport)].copy(), airport, ind))
+        ind += 1
+    split_df = tuple(split_df)
+    num_procs = 16
+    t = time.time()
+    pool = Pool(num_procs)
+    result = pool.map(compute_series, split_df)
+    final_df = pd.concat(result, ignore_index=True)
+    print(time.time()-t)
     pool.close()
     pool.join()
-    print(final_df)
     final_df["series"] = final_df["series"].astype(int)
+    print(final_df)
+
     final_df.to_csv("data/series_1.csv", index=False)
