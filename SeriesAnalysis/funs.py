c398be14e37de6ff190dc039610e091f8623e289
andrea <andygaspar@libero.it>
2021-04-13 17:58:29 +0200

    aggiunto gran fathers checker

--- SeriesAnalysis/funs.py
+++ SeriesAnalysis/funs.py
@@ -1,27 +1,30 @@
+import copy
+import datetime
 import time
 from itertools import product
 
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from multiprocess import Pool
 
+
 def compute(tup):
-    fl_dep, dep, time_tolerance, min_occurrences, call_icao, dep_arrive = tup
+    fl_list, df, time_tolerance, min_occurrences, call_icao, dep_arrive = tup
     regular = 0
-    for flight in fl_dep:
-        f = dep[dep[call_icao] == flight][dep_arrive]
+    print(df)
+    for flight in fl_list:
+        f = df[df[call_icao] == flight][dep_arrive]
         mean, std = f.mean(), f.std()
         if f[(f < mean + time_tolerance) & (f > mean - time_tolerance)].shape[0] / f.shape[0] > min_occurrences:
             regular += 1
     return regular
 
 
-def find_series_plot(df, call_icao, num_procs, dep_arrive, save=False):
+def find_series_plot(df, day, call_icao, num_procs, dep_arrive, save=False):
     tol = [30, 35, 40, 45, 50, 55, 60]
     min_oc = [0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
     grid = list(product(tol, min_oc))
-    day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
     for j in range(7):
         df_day = df[df["week day"] == j]
@@ -69,5 +72,63 @@ def find_series_plot(df, call_icao, num_procs, dep_arrive, save=False):
             plt.close()
         else:
             plt.show()
-
         print(j)
+
+
+def compute_series(tup):
+    fl_dep, dep, time_tolerance, min_occurrences, call_icao, dep_arrive = tup
+    columns = ['icao24', 'departure', 'arrival', 'callsign', 'day', 'week day', 'series code', 'series',
+               'airline', 'dep_or_arrival', 'mean_time']
+    series = pd.DataFrame(columns=columns)
+    for flight in fl_dep:
+        ser = dep[dep[call_icao] == flight]
+        f = ser[dep_arrive]
+        mean, std = f.mean(), f.std()
+        if f[(f < mean + time_tolerance) & (f > mean - time_tolerance)].shape[0] / f.shape[0] > min_occurrences:
+            to_append = ser[columns[:-2]].iloc[0].to_list() + [dep_arrive] + [mean]
+            series = series.append(dict(zip(columns, to_append)), ignore_index=True)
+
+    return series
+
+
+def find_series(df, day, tol, min_occurrence, call_icao, dep_arrive, num_procs=1):
+
+    df_day = df[df["week day"] == day]
+    flights_departure = df_day[call_icao].unique()
+    len_slice = flights_departure.shape[0] // num_procs
+    split_fl = [i * len_slice for i in range(num_procs)] + [flights_departure.shape[0]]
+
+    partial_time = time.time()
+
+    split_flights = tuple([(flights_departure[split_fl[i]:split_fl[i + 1]],
+                            df_day[df_day[call_icao].isin(flights_departure[split_fl[i]:split_fl[i + 1]])],
+                            tol, min_occurrence, call_icao, dep_arrive) for i in range(num_procs)])
+
+    pool = Pool(num_procs)
+    result = pool.map(compute_series, split_flights)
+    final_df = pd.concat(result, ignore_index=True)
+    pool.close()
+    pool.join()
+    print("paritial", time.time() - partial_time)
+
+    return final_df
+
+
+def get_departure_arrivals(df, airports):
+    departures = df[df["departure"].isin(airports["airport"])].copy().drop_duplicates()
+    arrivals = df[df["arrival"].isin(airports["airport"])].copy().drop_duplicates()
+
+    time_ = departures["dep time"].apply(
+        lambda d: datetime.datetime.fromtimestamp(d).time() if not np.isnan(d) else "NaN")
+    departures["dep time"] = time_
+    departures["dep time minute"] = time_.apply(lambda t: np.round(t.hour * 60 + t.minute + t.second * 0.1)).astype(int)
+
+    time_ = arrivals["arr time"].apply(
+        lambda d: datetime.datetime.fromtimestamp(d).time() if not np.isnan(d) else "NaN")
+    arrivals["arr time"] = time_
+    arrivals["arr time minute"] = time_.apply(lambda t: np.round(t.hour * 60 + t.minute + t.second * 0.1)).astype(int)
+
+    return departures, arrivals
+
+
+
--- SeriesAnalysis/series_analysis.py
+++ SeriesAnalysis/series_analysis.py
@@ -8,29 +8,61 @@ from multiprocess import Pool
 from itertools import product
 from SeriesAnalysis import funs as f
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
 num_procs = multiprocess.cpu_count()
 
 df = pd.read_csv('data/series_2019.csv')
 airports = pd.read_csv('data/airports.csv')
-departures = df[df["departure"].isin(airports["airport"])].copy().drop_duplicates()
-arrivals = df[df["arrival"].isin(airports["airport"])].copy().drop_duplicates()
+departures_2019, arrivals_2019 = f.get_departure_arrivals(df, airports)
 
-time_ = departures["dep time"].apply(lambda d: datetime.datetime.fromtimestamp(d).time() if not np.isnan(d) else "NaN")
-departures["dep time"] = time_
-departures["dep time minute"] = time_.apply(lambda t: np.round(t.hour * 60 + t.minute + t.second * 0.1)).astype(int)
-
-time_ = arrivals["arr time"].apply(lambda d: datetime.datetime.fromtimestamp(d).time() if not np.isnan(d) else "NaN")
-arrivals["arr time"] = time_
-arrivals["arr time minute"] = time_.apply(lambda t: np.round(t.hour * 60 + t.minute + t.second * 0.1)).astype(int)
 
 dep_arr = "dep time minute"
 call_icao = "callsign"
 
+# day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# tol = [30, 35, 40, 45, 50, 55, 60]
+# min_oc = [0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
 
-f.find_series_plot(departures, call_icao, num_procs, dep_arr, save=False)
+day = 0
+tol = 30
+max_occurence = 0.7
+# f.find_series_plot(departures, day, call_icao, num_procs, dep_arr, save=False)
 
 
+series_2019 = f.find_series(departures_2019, day, tol, max_occurence, call_icao, dep_arr, num_procs)
 
 
+df = pd.read_csv('data/series_2018.csv')
+airports = pd.read_csv('data/airports.csv')
+departures_2018, arrivals_2018 = f.get_departure_arrivals(df, airports)
+
+series_2018 = f.find_series(departures_2018, day, tol, max_occurence, call_icao, dep_arr, num_procs)
 
 
+def gran_fathers(flight):
+    dep = series_2019[series_2019.callsign == flight]["departure"].unique()[0]
+    arr = series_2019[series_2019.callsign == flight]["arrival"].unique()[0]
+    air = series_2019[series_2019.callsign == flight]["airline"].unique()[0]
+    t = series_2019[series_2019.callsign == flight]["mean_time"].unique()[0]
+    df_fl = series_2018[(series_2018.departure == dep) & (series_2018.arrival == arr) & (series_2018.airline == air)
+                        & (series_2018.mean_time >= t-gf_tolerance) & (series_2018.mean_time <= t+gf_tolerance)]
+    if df_fl.shape[0]>0:
+        return 1
+    else:
+        return 0
+
+
+gf_tolerance= 30
+gf_rules = 0
+
+tt = time.time()
+
+pool = Pool(num_procs)
+result = sum(pool.map(gran_fathers, series_2019.callsign.unique()))
+pool.close()
+pool.join()
+
+
+print(time.time() - tt)
